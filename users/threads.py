5522ebe0d799fb468338e90d09c7db0795bddf2f
Aayog Koirala <1aayogkoirala@gmail.com>
2023-03-01 21:46:43 -0500

    reformatted, modified newspost fields and permissions added favorite reporters/liking/reporting

---
+++ users/threads.py
@@ -0,0 +1,22 @@
+import threading
+
+# from django.core.mail import send_mail
+from django.conf import settings
+from django.core.mail import EmailMessage
+
+
+class EmailThread(threading.Thread):
+    def __init__(self, subject, message, recipient_list):
+        self.subject = subject
+        self.message = message
+        self.recipient_list = recipient_list
+        threading.Thread.__init__(self)
+
+    def run(self):
+        email = EmailMessage(self.subject, self.message, to=self.recipient_list)
+        email.content_subtype = "html"
+        email.send()
+
+
+def send_email(subject, message, recipient_list):
+    EmailThread(subject, message, recipient_list).start()
